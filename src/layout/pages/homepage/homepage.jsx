a7d65eda643bcdd030c40ec5f2b64f4fc25429be
Anas Alam <anasalam027@gmail.com>
2020-10-02 03:05:38 +0530

    hero Section complete

--- src/layout/pages/homepage/homepage.jsx
+++ src/layout/pages/homepage/homepage.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./homepage.css";
 
-export default class Homepage extends React.Component{
-    render(){
-        return(
-            <div className="homepage-container"></div>
-        )
-    }
+export default class Homepage extends React.Component {
+  render() {
+    return (
+      <div className="homepage-container">
+          <div className="heroText">
+            <p>One stop for all your Car related problems</p>
+            <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora,
+            dicta culpa non quis numquam natus
+            </p>
+          </div>
+      </div>
+    );
+  }
 }
