d491187175e0f41e957448c96a7c2fd61fc197c4
sibghat <53030222+sibghat0@users.noreply.github.com>
2020-10-13 23:09:30 +0530

    sss

--- src/layout/components/navbar/navbar.jsx
+++ src/layout/components/navbar/navbar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./navbar.css";
+import logo from '../../../assets/light_ultimate.png'
 
 export default class Navbar extends React.Component{
     render(){
         return(
             <div className="navbar">
                 <div className="logo">
-                    <p>Super <span>Service</span></p>
+                    <img src= {logo} alt="image"/>
                 </div>
                 <nav>
                     <ul>
