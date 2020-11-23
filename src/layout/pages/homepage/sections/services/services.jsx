2d4cdf014e03ddabf3f4319037b496d79df7cc7a
sibghat <53030222+sibghat0@users.noreply.github.com>
2020-11-23 12:37:06 +0530

    sibghat

--- src/layout/pages/homepage/sections/services/services.jsx
+++ src/layout/pages/homepage/sections/services/services.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import './services.css';
+import "./services.css";
+import map from "../../../../../assets/indiamap.png";
 
-export default class Services extends React.Component{
-    render(){
-        return(
-            <div className="services">
-                <div className="h2">Services we Offer</div>
-            </div>
-        )
-    }
+export default class Services extends React.Component {
+  render() {
+    return (
+      <div className="services">
+        <div className="state">
+          <div className="map">
+            <img src={map} alt="map" />
+          </div>
+          <div className="state-name"></div>
+        </div>
+      </div>
+    );
+  }
 }
