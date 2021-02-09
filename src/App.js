664fb93f7f23100e317ca0318a0f746a24215afa
Anas Alam <anasalam027@gmail.com>
2021-02-09 20:37:17 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
 import { Switch, Route, useLocation } from "react-router-dom";
+import Navbar from "./layout/components/navbar/navbar";
 
 function App() {
   const location = useLocation();
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
--- src/layout/components/navbar/navbar.js
+++ src/layout/components/navbar/navbar.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
 import "./navbar.css";
+import logo from "../../../assets/logo.png";
 
 export default class Navbar extends Component {
   render() {
-    return <div className="navbar"></div>;
+    return (
+      <div className="navbar">
+        <img src={logo} alt="" />
+        <div className="nav">
+          <a href="/">Home</a>
+          <a href="/">Vehicles</a>
+          <a href="/">Review</a>
+          <a href="/">Tour</a>
+          <a href="/">Travels</a>
+          <a href="/">About</a>
+        </div>
+      </div>
+    );
   }
 }
