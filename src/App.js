9d4083b5fa31db03e81cc6a1162452fd2f3e87fe
Anas Alam <anasalam027@gmail.com>
2021-02-09 20:16:42 +0530

    renew

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { Switch, Route, useLocation } from "react-router-dom";
-import Navbar from './layout/components/navbar/navbar';
-import Homepage from './layout/pages/homepage/homepage';
 
 function App() {
   const location = useLocation();
-  return (
-    <div className="App">
-      <Navbar />
-      <Switch location={location} key={location.pathname}>
-          <Route exact path="/" component={Homepage}/>
-      </Switch>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
