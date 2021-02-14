30d18a525330836466dba63b7a827373fcdad71d
sibghat <53030222+sibghat0@users.noreply.github.com>
2021-02-14 14:41:27 +0530

    travel

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Navbar from "./layout/components/navbar/navbar";
 import Home from "./layout/pages/home/home";
 import Footer from "./layout/components/footer/footer";
 import Cars from "./layout/pages/cars/cars";
-import TourMe from "./layout/pages/tour/tour";
 import TravelMe from "./layout/pages/travel/travel";
 import Contact from "./layout/pages/contact/contact";
+import Tourme from "./layout/pages/tour/tour";
 
 function App() {
   const location = useLocation();
@@ -17,8 +17,8 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/vehicles" component={Cars} />
-        <Route exact path="/tour" component={TourMe} />
-        <Route exact path="/tour" component={TravelMe} />
+        <Route exact path="/tour" component={Tourme} />
+        <Route exact path="/travel" component={TravelMe} />
         <Route exact path="/contact" component={Contact} />
       </Switch>
       <Footer />
