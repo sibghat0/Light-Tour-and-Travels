3850c67ab80a0fe66a5e94a0c4de397d92bc7fae
sibghat <53030222+sibghat0@users.noreply.github.com>
2021-04-07 14:47:57 +0530

    DONE

--- src/layout/pages/cars/cars.jsx
+++ src/layout/pages/cars/cars.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import "./cars.css";
 
-import car1 from "../../../assets/car1.png";
+import car1 from "../../../assets/car1.jpg";
 import car2 from "../../../assets/car2.jpg";
-import car3 from "../../../assets/car3.jpg";
-import car4 from "../../../assets/car4.jpg";
+import car3 from "../../../assets/car3.png";
+import car4 from "../../../assets/car4.png";
 
 export default class Cars extends Component {
   constructor(props) {
@@ -14,10 +14,8 @@ export default class Cars extends Component {
       data: [
         {
           img: car1,
-          h2: "INNOVA",
-          p: `The Toyota Innova has 1 Diesel Engine and 1 Petrol Engine on offer. The Diesel engine is 2494 cc while the Petrol engine is 
-              1998 cc . It is available with the Manual transmission. Depending upon the variant and fuel type the Innova has a mileage of 11.4 to 12.99 kmpl. The Innova is a 8 seater 
-              and has length of 4585mm, width of 1760mm and a wheelbase of 2750mm.`,
+          h2: "SWIFT DEZIRE",
+          p: `The Maruti Dzire has 1 Petrol Engine on offer. The Petrol engine is 1197 cc . ... Depending upon the variant and fuel type the Dzire has a mileage of 23.26 to 24.12 kmpl. The Dzire is a 5 seater and has length of 3995mm, width of 1735mm and a wheelbase of 2450mm.`,
         },
         {
           img: car2,
@@ -35,17 +33,8 @@ export default class Cars extends Component {
         },
         {
           img: car4,
-          h2: "Web Maintanance",
-          p: `We understand your dilemma! You want the website to
-          keep performing well and stay fruitful to you. At
-          Intlum, we take pride in maintaining the website on
-          your behalf, so that you don’t need to worry about the
-          engagement and optimization of your website. We keep
-          the flow of leads coming to our clients by maintaining
-          their websites and we want to keep on doing so by
-          maintaining the website and helping you retain the
-          leads for your business and keeping its standard
-          alive.`,
+          h2: "TEMPO TRAVELLER",
+          p: `The Force Traveller is a light commercial vehicle made and distributed by Indian manufacturer Force Motors. The Traveller is powered by a Mercedes-Benz derived 2.6-litre (2596 cc) FM 2.6 CR BS VI engine, with CNG and diesel options. The diesel unit produces 67 kW (91 PS; 90 bhp) @ 2800 rpm and torque of 250 N⋅m (184 lb⋅ft) @ 1400 - 2400 rpm.`,
         },
       ],
     };
@@ -54,7 +43,7 @@ export default class Cars extends Component {
   render() {
     return (
       <div className="services">
-        <h1>Cars We Provide</h1>
+        <h1>CARS WE PROVIDE</h1>
         <div className="services-body">
           {this.state.data.map((item) => {
             return (
